595e958c62be5e8ba807e0d7cadbc89122d76bd4
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-12-08 03:10:09 +0530

    Created Login and Browe Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './Compoents/Body';
 
 function App() {
   return (
-    <div className="text-3xl bold text-red-600">
-      Hii Eveyone Let's get started!
+    <div >
+      <Body/>
     </div>
+    
   );
 }
 
---
+++ src/Compoents/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+function Login() {
+  return (
+    <div >
+        <Header/>
+        <div>
+            <img src = "https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+             alt="Background" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Login
