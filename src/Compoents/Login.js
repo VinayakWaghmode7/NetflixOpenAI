5153065e71cbeacef78c02d2fb71bbe099224ee0
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-12-08 03:45:25 +0530

    Created Login and Header

--- src/Compoents/Login.js
+++ src/Compoents/Login.js
@@ -5,10 +5,16 @@ function Login() {
   return (
     <div >
         <Header/>
-        <div>
+        <div className='absolute'>
             <img src = "https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg"
              alt="Background" />
         </div>
+        <form className='w-3/12 text-white p-8 bg-black absolute my-36 mx-auto right-0 left-0 rounded-lg bg-opacity-80'>
+            <h1 className='font-bold text-3xl py-4 '>Sign In</h1>
+            <input type="text" placeholder="Email" className='p-4 my-4 w-full bg-gray-700'/>
+            <input type="Password" placeholder='Password' className='p-4 my-4 w-full bg-gray-700'/>
+            <button className='p-4 my-6 bg-red-700 w-full'>Sign In</button>
+        </form>
 
     </div>
   )
