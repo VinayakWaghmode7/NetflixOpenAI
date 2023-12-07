7a13cf40fb202fdb7b0c4057adbcc4fe56800624
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-12-08 01:31:03 +0530

    Netflix Draw Structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl bold text-red-600">
       Hii Eveyone Let's get started!
     </div>
   );
