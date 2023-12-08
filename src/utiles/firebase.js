dc1bfed3089bce29e329c3a60b03ec2974a8d463
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-12-09 00:50:08 +0530

    Added the Firebase in our App

---
+++ src/utiles/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBE6TTdToMXNV3Cz4meLcFUkylsuyUoSzw",
+  authDomain: "netflixopenai.firebaseapp.com",
+  projectId: "netflixopenai",
+  storageBucket: "netflixopenai.appspot.com",
+  messagingSenderId: "1049286689081",
+  appId: "1:1049286689081:web:564120b7d3f694d96668e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
